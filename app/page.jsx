bc4e881f4e5f1305ed58aa32d4fd12df32eaa549
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-25 14:37:31 +0200

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,93 @@
 export default function HomePage() {
   return (
     <>
-      <div className="flex flex-col items-center  justify-center m-10">
-        <div className="border-black border-x-2 p-2">
-          <label className="text-2xl font-semibold font-sans" htmlFor="">
-            What is this app for ?
-          </label>
-          <h1>
-            This is a web app developed, so it can help you track your routine
-            and be a better version of yourself.
-          </h1>
+      <div className="flex flex-col items-center gap-20 justify-center w-300 mx-auto py-10 border-black border-5 mt-10">
+        <h1 className="text-2xl font-bold mb-10">Questions</h1>
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              What is this app for ?
+            </label>
+            <h1>
+              This is a web app developed, so it can help you track your routine
+              and be a better version of yourself.
+            </h1>
+          </div>
+        </div>
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              How can I use it right?
+            </label>
+            <h1>
+              It's really simple. First you have to create an account. Second
+              you must answer sincerely on some questions about you. And third,
+              every day you track your daily routine checking the appropriate
+              box.
+            </h1>
+          </div>
+        </div>
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              Should i create an account first?
+            </label>
+            <h1>
+              Yes. You must create an account. Dont worry it only lasts some
+              seconds and it's really simple.
+            </h1>
+          </div>
+        </div>{" "}
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              Should i create an account first?
+            </label>
+            <h1>
+              Yes. You must create an account. Dont worry it only lasts some
+              seconds and it's really simple.
+            </h1>
+          </div>
+        </div>{" "}
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              Should i create an account first?
+            </label>
+            <h1>
+              Yes. You must create an account. Dont worry it only lasts some
+              seconds and it's really simple.
+            </h1>
+          </div>
+        </div>{" "}
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              Should i create an account first?
+            </label>
+            <h1>
+              Yes. You must create an account. Dont worry it only lasts some
+              seconds and it's really simple.
+            </h1>
+          </div>
+        </div>{" "}
+        <div className="flex justify-start items-center w-full max-w-3xl gap-2">
+          <p className="bg-black w-2 h-2 rounded-2xl"></p>
+          <div className="border-black border-x-2 p-2 w-full">
+            <label className="text-2xl font-semibold font-sans">
+              Should i create an account first?
+            </label>
+            <h1>
+              Yes. You must create an account. Dont worry it only lasts some
+              seconds and it's really simple.
+            </h1>
+          </div>
         </div>
       </div>
     </>
