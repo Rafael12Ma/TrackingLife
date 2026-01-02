cf143b672bda3c339d751fdf18ca8a9d558ba8eb
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2026-01-02 17:35:19 +0200

    setting not found and error pages

--- app/error.jsx
+++ app/error.jsx
@@ -1,10 +1,19 @@
 "use client";
 
+import Link from "next/link";
+
 export default function ErroPage() {
   return (
     <>
-      <div>
-        <h1>An error has occured</h1>
+      <div className="flex flex-col justify-center items-center m-30">
+        <h1 className="text-red-500 font-semibold text-[2rem]">ERROR 404</h1>
+        <h1 className="m-4">An error has occured !</h1>
+        <Link
+          href="/tracks"
+          className="font-semibold hover:scale-110 transition duration-200 cursor-pointer text-blue-500"
+        >
+          Go to Tracks
+        </Link>
       </div>
     </>
   );
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,27 @@
+import peter from "@/public/peter.jpg";
+import Link from "next/link";
+
 export default function Notfound() {
   return (
     <>
-      <div>
-        <h1>Page not found!</h1>
+      <div className="flex flex-col gap-5 justify-center items-center m-20">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <h1 className="font-mono font-semibold text-lg">
+            Maybe you went too far surfing?
+          </h1>
+          <Link
+            href="/tracks"
+            className="font-semibold hover:scale-110 transition duration-200 cursor-pointer text-blue-500"
+          >
+            Go to Tracks
+          </Link>
+        </div>
+        <img
+          className="rounded-xl shadow-black"
+          src={peter.src}
+          width={600}
+          alt="Peter Griffin surfing way to far."
+        />
       </div>
     </>
   );
