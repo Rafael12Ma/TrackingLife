39b9ec73cfce8376fcb4c924f34c8aa859219da1
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2026-01-06 20:59:23 +0200

    New habits set

--- Helper-Components/about/form-submit/habits.js
+++ Helper-Components/about/form-submit/habits.js
@@ -1,10 +1,8 @@
 export const habits = [
     "Wake up early",
-    "Go to work",
     "Read a book",
     "Eat healthy",
     "Workout",
     "Study",
-    "Practice coding",
     "Sleep early",
 ];
