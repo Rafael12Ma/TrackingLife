0e2e33aa77994c55897bb6ede73a64fe6f10e1f8
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2026-01-06 21:27:32 +0200

    time settling

--- Helper-Components/about/form-submit/habits.js
+++ Helper-Components/about/form-submit/habits.js
@@ -1,11 +1,11 @@
 export const habits = [
     "Wake up early",
-    "Read a book",
+    "Read a book (>30m)",
     "Eat healthy",
     "Workout",
-    "Study",
-    "Sleep early",
+    "Study (>60m)",
+    "Sleep early (< 24:00)",
     "Cold Shower",
-    "Social M. Detox",
+    "Social M. Detox (<40m)",
     "No alcohol"
 ];
